14f12a862493fe95b55d18136daeb7be48e4ceba
Carlos Sierra <carlossierra850@gmail.com>
2022-08-02 23:18:59 -0400

    adding middleware auth (redirect)

--- middleware.js
+++ middleware.js
@@ -4,7 +4,7 @@ export default function middleware(req) {
   const { cookies } = req;
   const token = cookies.get("token");
   if (!token) {
-    return NextResponse.rewrite(new URL("/login", req.url));
+    return NextResponse.redirect(new URL("/login", req.url));
   }
 }
 export const config = {
