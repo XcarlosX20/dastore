c7c1d28de3c714de2ddac83871f95d508ddc3656
Carlos Sierra <carlossierra850@gmail.com>
2022-09-24 23:41:52 -0400

    => commit

--- pages/login.js
+++ pages/login.js
@@ -6,75 +6,76 @@ import {
   OutlinedInput,
   InputAdornment,
   IconButton,
-  Button
-} from '@mui/material'
-import Loading from '../Components/Utils/Loading'
-import { Visibility, VisibilityOff } from '@mui/icons-material'
-import { authCompanyAction } from '../Actions/ActionsAuth'
+  Button,
+} from "@mui/material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import Loading from "../Components/Utils/Loading";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { authCompanyAction } from "../Actions/ActionsAuth";
 // import { showAlertAction } from "../Actions/ActionsAlert";
-import { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import Swal from 'sweetalert2'
-import { useRouter } from 'next/router'
-import Image from 'next/image'
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Swal from "sweetalert2";
+import { useRouter } from "next/router";
+import Image from "next/image";
 const Login = () => {
-  const { loading, auth } = useSelector((state) => state.auth)
-  const dispatch = useDispatch()
-  const router = useRouter()
-  const authCompany = (company) => dispatch(authCompanyAction(company))
+  const { loading, auth } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const authCompany = (company) => dispatch(authCompanyAction(company));
   // const showAlert = (alert) => dispatch(showAlertAction(alert));
-  const { alert } = useSelector((state) => state.alert)
+  const { alert } = useSelector((state) => state.alert);
   const [values, setValues] = useState({
-    companyEmail: '',
-    password: '',
-    showPassword: false
-  })
+    companyEmail: "",
+    password: "",
+    showPassword: false,
+  });
   const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value })
-  }
+    setValues({ ...values, [prop]: event.target.value });
+  };
   const handleClickShowPassword = () => {
     setValues({
       ...values,
-      showPassword: !values.showPassword
-    })
-  }
+      showPassword: !values.showPassword,
+    });
+  };
   // for email validation: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)/.test(values.email)
   const handleMouseDownPassword = (event) => {
-    event.preventDefault()
-  }
+    event.preventDefault();
+  };
   const login = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (values.password && values.email) {
       const company = {
         companyEmail: values.email,
-        password: values.password
-      }
-      await authCompany(company)
+        password: values.password,
+      };
+      await authCompany(company);
     } else {
       Swal.fire({
-        title: 'all fields are required',
-        icon: 'info'
-      })
+        title: "all fields are required",
+        icon: "info",
+      });
     }
-  }
+  };
   useEffect(() => {
     if (auth) {
-      router.push('/products')
+      router.push("/products");
     }
-  }, [auth])
+  }, [auth]);
   const dialog = {
     zIndex: 10,
-    position: 'fixed',
-    top: '50%',
-    left: '50%',
-    width: '100%',
-    transform: 'translate(-50%, -50%)',
-    bgcolor: 'transparent'
-  }
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    width: "100%",
+    transform: "translate(-50%, -50%)",
+    bgcolor: "transparent",
+  };
   return (
     <>
       {alert != null && (
-        <div className={alert.class} role='alert'>
+        <div className={alert.class} role="alert">
           {alert.txt}
         </div>
       )}
@@ -85,24 +86,24 @@ const Login = () => {
       )}
       <Grid container spacing={2}>
         <Grid item xs={12} md={6}>
-          <Box className='login trapecio'>
+          <Box className="login trapecio">
             <Image
-              height='100%'
-              width='100%'
-              layout='responsive'
-              src='/login-img.png'
-              alt='login'
+              height="100%"
+              width="100%"
+              layout="responsive"
+              src="/login-img.png"
+              alt="login"
             />
           </Box>
         </Grid>
         <Grid item xs={12} md={6}>
           <Grid
-            paddingX='5rem'
+            paddingX="5rem"
             container
-            direction='column'
-            justifyContent='center'
-            alignItems='center'
-            height='100%'
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            height="100%"
           >
             <form onSubmit={login}>
               <Grid container rowGap={2}>
@@ -110,50 +111,60 @@ const Login = () => {
                 <FormControl fullWidth>
                   <InputLabel>Email</InputLabel>
                   <OutlinedInput
-                    label='Email'
-                    onChange={handleChange('email')}
+                    label="Email"
+                    onChange={handleChange("email")}
                   />
                 </FormControl>
-                <FormControl fullWidth variant='outlined'>
-                  <InputLabel htmlFor='outlined-adornment-password'>
+                <FormControl fullWidth variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-password">
                     Password
                   </InputLabel>
                   <OutlinedInput
-                    id='outlined-adornment-password'
-                    type={values.showPassword ? 'text' : 'password'}
+                    id="outlined-adornment-password"
+                    type={values.showPassword ? "text" : "password"}
                     value={values.password}
-                    onChange={handleChange('password')}
+                    onChange={handleChange("password")}
                     endAdornment={
-                      <InputAdornment position='end'>
+                      <InputAdornment position="end">
                         <IconButton
-                          aria-label='toggle password visibility'
+                          aria-label="toggle password visibility"
                           onClick={handleClickShowPassword}
                           onMouseDown={handleMouseDownPassword}
-                          edge='end'
+                          edge="end"
                         >
-                          {values.showPassword
-                            ? (
-                              <VisibilityOff />
-                              )
-                            : (
-                              <Visibility />
-                              )}
+                          {values.showPassword ? (
+                            <VisibilityOff />
+                          ) : (
+                            <Visibility />
+                          )}
                         </IconButton>
                       </InputAdornment>
                     }
-                    label='Password'
+                    label="Password"
                   />
                 </FormControl>
-                <Button variant='contained' color='info' type='submit'>
+                <Button variant="contained" color="info" type="submit">
                   Login
                 </Button>
               </Grid>
             </form>
+            <Button
+              startIcon={<VisibilityIcon />}
+              sx={{ marginTop: "0.5rem" }}
+              onClick={async () => {
+                await authCompany({
+                  companyEmail: "admin",
+                  password: "admin",
+                });
+              }}
+            >
+              Sign in as a guest
+            </Button>
           </Grid>
         </Grid>
       </Grid>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
