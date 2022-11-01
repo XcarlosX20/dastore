dbc63d74c1f4b948045e5d0ea568c6ef217392a7
Carlos Sierra <carlossierra850@gmail.com>
2022-11-01 13:45:38 -0400

    deploy test (socket)

--- config/socket/index.js
+++ config/socket/index.js
@@ -1,10 +1,5 @@
 import { io } from "socket.io-client";
-const backendApi = new URL(
-  process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_MYAPP_BACKEND
-    : process.env.NEXT_PUBLIC_MYAPP_BACKEND_LOCAL
-);
-const socket = io(backendApi.origin + ":" + NEXT_PUBLIC_SOCKET_PORT, {
+const socket = io(process.env.NEXT_PUBLIC_SOCKET_ENDPOINT, {
   reconnectionDelayMax: 10000,
 });
 socket.open();
