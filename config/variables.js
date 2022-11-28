af3e8a73a28ce3eb5110b3a71113492b56cbb0e7
Carlos Sierra <carlossierra850@gmail.com>
2022-11-28 02:33:04 -0400

    change envairoment

--- config/variables.js
+++ config/variables.js
@@ -1,9 +1,10 @@
-const modeEnv = process.env.NODE_ENV === 'development'
+const modeEnv = process.env.NODE_ENV === "production";
 export const SOCKET_ENDPOINT = modeEnv
-  ? process.env.NEXT_PUBLIC_SOCKET_ENDPOINT_LOCAL
-  : process.env.NEXT_PUBLIC_SOCKET_ENDPOINT
+  ? process.env.NEXT_PUBLIC_SOCKET_ENDPOINT
+  : process.env.NEXT_PUBLIC_SOCKET_ENDPOINT_LOCAL;
+
 export const backendApi = new URL(
-  process.env.NODE_ENV === 'development'
-    ? process.env.NEXT_PUBLIC_MYAPP_BACKEND_LOCAL
+  process.env.NODE_ENV === "production"
+    ? process.env.NEXT_PUBLIC_MYAPP_BACKEND
     : process.env.NEXT_PUBLIC_MYAPP_BACKEND_LOCAL
-).origin
+).origin;
