8409dafd7821eba786ac863c69f0c004db34c74e
Carlos Sierra <carlossierra850@gmail.com>
2024-04-13 02:16:39 -0400

    api url was changed

--- Services/dolarTodayApi.js
+++ Services/dolarTodayApi.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 export const dolarTodayApi = async () => {
   const response = await axios.get(process.env.NEXT_PUBLIC_MYAPP_USD_TO_BS)
-  return response.data.USD.promedio_real
+  return response.data.price
 }
